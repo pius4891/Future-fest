14325a6436703da7c6c110b66d22d536b8392edd
pius4891 <eliudmutwiri047@gmail.com>
2026-02-21 11:25:56 +0300

    Add new backend and images

--- Backed Logic spark/src/routes/contact.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { createContact } = require("../controllers/contact.controller");
-
-router.post("/", createContact);
-
-module.exports = router;
